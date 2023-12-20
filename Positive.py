8bad38cd839aa3539ae5e3f8482a0750558d5a63
Aravind <saiaravind22srinivasulu@gmail.com>
2023-12-20 16:45:43 +0530

    sec commit

---
+++ Positive.py
@@ -0,0 +1,8 @@
+num1=eval(input("Enter a number :"))
+
+if(num1>0):
+    num1=num1+num1
+    print("After adding two times",num1)
+else:
+    num1=num1*num1
+    print("After sqauring by itself", num1)
