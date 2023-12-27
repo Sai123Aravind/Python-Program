77d818f2bab31db9ca029fbe30c05e622e84741e
Aravind <saiaravind22srinivasulu@gmail.com>
2023-12-27 09:09:51 +0530

    fourth commit

--- exam.py
+++ exam.py
@@ -1,12 +1,5 @@
-flag=""
-a=0
-i=1
-while(a<3):
-
-    j=1
-    if flag:
-        i=j*i+5
-    else:
-        i=j*i+1
-    a=a+1
-print(i)
+def fn(var1):
+    var1.pop(1)
+var1=[1,2,3]
+fn(var1)
+print(var1)
